42cdd9a56ff4ed3c980caff2dfe94fcc372d3d0c
Adaeze-Tochukwu <adarose.tochukwu@gmail.com>
2023-06-27 10:19:26 +0100

    finsished the project

--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./Subscribe.scss";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Button from "../button/Button"
 
 export default function Subscribe() {
   return (
     <div className="subscribe">
-      <div className="subscribe__content">
-        <h2>Subscribe newsletter</h2>
-        <p>
-          By following these three steps - proper watering, appropriate
-          sunlight, and providing essential nutrients - you'll be well on your
-          way to maintaining healthy and thriving plants.
-        </p>
-        <form>
-          <input type="text" placeholder="Enter your email" />
-          <Link>Subscribe</Link>
-        </form>
+      <div className='subscribe__backgroundImage'>
+        <div className="subscribe__content">
+          <h2>Subscribe newsletter</h2>
+          <p>
+            By following these three steps - proper watering, appropriate
+            sunlight, and providing essential nutrients - you'll be well on your
+            way to maintaining healthy and thriving plants.
+          </p>
+          <form>
+            <input type="text" placeholder="Enter your email" />
+            <Button button="Subscribe"/>
+          </form>
+        </div>
       </div>
     </div>
   );
